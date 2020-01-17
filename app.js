ce55832630a333465456651693c1f81601f5b819
Aaron Yuan <aaronyuan27@gmail.com>
2020-01-16 17:02:14 -0700

    updated log display

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+        turns:[],
     },
     methods:{
         startGame: function(){
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
 
         attack: function(){
-            this.monsterHealth -= this.calculateDamage(3,10);
+            var damage = this.calculateDamage(3,10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster for' + damage
+            });
             if(this.checkWin()){
                 return;
             }
@@ -23,21 +30,35 @@ new Vue({
         },
 
         specialAttack: function(){
-            this.monsterHealth -= this.calculateDamage(10,20);
+            var damage = this.calculateDamage(10,20)
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster with special attack for' + damage
+            });
             if(this.checkWin()){
                 return;
             }
-
             this.monsterAttacks();
             
         },
 
         heal: function(){
-
+            if(this.playerHealth <= 90){
+            this.playerHealth += 10;
+            }
+            else {
+                this.playerHealth = 100;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for' + damage
+            });
+            this.monsterAttacks();
         },
 
         giveUp: function(){
-
+            this.gameIsRunning = false;
         },
 
         calculateDamage: function (min ,max){
@@ -65,8 +86,13 @@ new Vue({
         },
 
         monsterAttacks: function(){
-            this.playerHealth -= this.calculateDamage(5,12);
+            var damage = this.calculateDamage(5,12);
+            this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits player for' + damage
+            });
         }
     }
 });
